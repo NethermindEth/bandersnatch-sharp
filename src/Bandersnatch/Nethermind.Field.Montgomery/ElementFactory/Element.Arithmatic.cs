bf244d05b12e737cf7bd0e1165626819d3dddec2
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-01-19 18:10:09 +0530

    implement additional functions

--- src/Bandersnatch/Nethermind.Field.Montgomery/ElementFactory/Element.Arithmatic.cs
+++ src/Bandersnatch/Nethermind.Field.Montgomery/ElementFactory/Element.Arithmatic.cs
@@ -1,5 +1,8 @@
 using System.Data;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using Nethermind.Int256;
 using FE = Nethermind.Field.Montgomery.ElementFactory.Element;
 
@@ -291,6 +294,140 @@ namespace Nethermind.Field.Montgomery.ElementFactory
         private void Rsh192(out FE res)
         {
             res = new FE(u3);
+        }
+
+        public static bool SubtractUnderflow(in FE a, in FE b, out FE res)
+        {
+            ref ulong rx = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in a));
+            ref ulong ry = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in b));
+            ulong borrow = 0ul;
+            ElementUtils.SubtractWithBorrow(rx, ry, ref borrow, out ulong res0);
+            ElementUtils.SubtractWithBorrow(Unsafe.Add(ref rx, 1), Unsafe.Add(ref ry, 1), ref borrow, out ulong res1);
+            ElementUtils.SubtractWithBorrow(Unsafe.Add(ref rx, 2), Unsafe.Add(ref ry, 2), ref borrow, out ulong res2);
+            ElementUtils.SubtractWithBorrow(Unsafe.Add(ref rx, 3), Unsafe.Add(ref ry, 3), ref borrow, out ulong res3);
+            res = new FE(res0, res1, res2, res3);
+            return borrow != 0;
+        }
+
+        public static bool AddOverflow(in FE a, in FE b, out FE res)
+        {
+            ref ulong rx = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in a));
+            ref ulong ry = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in b));
+            ulong carry = 0ul;
+            ElementUtils.AddWithCarry(rx, ry, ref carry, out ulong res0);
+            ElementUtils.AddWithCarry(Unsafe.Add(ref rx, 1), Unsafe.Add(ref ry, 1), ref carry, out ulong res1);
+            ElementUtils.AddWithCarry(Unsafe.Add(ref rx, 2), Unsafe.Add(ref ry, 2), ref carry, out ulong res2);
+            ElementUtils.AddWithCarry(Unsafe.Add(ref rx, 3), Unsafe.Add(ref ry, 3), ref carry, out ulong res3);
+            res = new FE(res0, res1, res2, res3);
+            return carry != 0;
+        }
+
+        public static bool AddImpl(in FE a, in FE b, out FE res)
+        {
+            if (Avx2.IsSupported)
+            {
+                Vector256<ulong> av = Unsafe.As<FE,Vector256<ulong>>(ref Unsafe.AsRef(in a));
+                Vector256<ulong> bv = Unsafe.As<FE,Vector256<ulong>>(ref Unsafe.AsRef(in b));
+
+                Vector256<ulong> result = Avx2.Add(av, bv);
+                Vector256<ulong> carryFromBothHighBits = Avx2.And(av, bv);
+                Vector256<ulong> eitherHighBit = Avx2.Or(av, bv);
+                Vector256<ulong> highBitNotInResult = Avx2.AndNot(result, eitherHighBit);
+
+                // Set high bits where carry occurs
+                Vector256<ulong> vCarry = Avx2.Or(carryFromBothHighBits, highBitNotInResult);
+                // Move carry from Vector space to int
+                int carry = Avx.MoveMask(Unsafe.As<Vector256<ulong>, Vector256<double>>(ref vCarry));
+
+                // All bits set will cascade another carry when carry is added to it
+                Vector256<ulong> vCascade = Avx2.CompareEqual(result, Vector256<ulong>.AllBitsSet);
+                // Move cascade from Vector space to int
+                int cascade = Avx.MoveMask(Unsafe.As<Vector256<ulong>, Vector256<double>>(ref vCascade));
+
+                // Use ints to work out the Vector cross lane cascades
+                // Move carry to next bit and add cascade
+                carry = cascade + 2 * carry; // lea
+                // Remove cascades not effected by carry
+                cascade ^= carry;
+                // Choice of 16 vectors
+                cascade &= 0x0f;
+
+                // Lookup the carries to broadcast to the Vectors
+                Vector256<ulong> cascadedCarries = Unsafe.Add(ref Unsafe.As<byte, Vector256<ulong>>(ref MemoryMarshal.GetReference(ElementUtils.SBroadcastLookup)), cascade);
+
+                // Mark res as initalized so we can use it as left said of ref assignment
+                Unsafe.SkipInit(out res);
+                // Add the cascadedCarries to the result
+                Unsafe.As<FE,Vector256<ulong>>(ref res) = Avx2.Add(result, cascadedCarries);
+                return (carry & 0b1_0000) != 0;
+            }
+            else
+            {
+                ref ulong rx = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in a));
+                ref ulong ry = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in b));
+                ulong carry = 0ul;
+                ElementUtils.AddWithCarry(rx, ry, ref carry, out ulong res0);
+                ElementUtils.AddWithCarry(Unsafe.Add(ref rx, 1), Unsafe.Add(ref ry, 1), ref carry, out ulong res1);
+                ElementUtils.AddWithCarry(Unsafe.Add(ref rx, 2), Unsafe.Add(ref ry, 2), ref carry, out ulong res2);
+                ElementUtils.AddWithCarry(Unsafe.Add(ref rx, 3), Unsafe.Add(ref ry, 3), ref carry, out ulong res3);
+                res = new FE(res0, res1, res2, res3);
+                return carry != 0;
+            }
+        }
+
+        public static bool SubtractImpl(in FE a, in FE b, out FE res)
+        {
+            if (Avx2.IsSupported)
+            {
+                Vector256<ulong> av = Unsafe.As<FE, Vector256<ulong>>(ref Unsafe.AsRef(in a));
+                Vector256<ulong> bv = Unsafe.As<FE, Vector256<ulong>>(ref Unsafe.AsRef(in b));
+
+                Vector256<ulong> result = Avx2.Subtract(av, bv);
+                // Invert top bits as Avx2.CompareGreaterThan is only available for longs, not unsigned
+                Vector256<ulong> resultSigned = Avx2.Xor(result, Vector256.Create<ulong>(0x8000_0000_0000_0000));
+                Vector256<ulong> avSigned = Avx2.Xor(av, Vector256.Create<ulong>(0x8000_0000_0000_0000));
+
+                // Which vectors need to borrow from the next
+                Vector256<long> vBorrow = Avx2.CompareGreaterThan(Unsafe.As<Vector256<ulong>, Vector256<long>>(ref resultSigned),
+                                                      Unsafe.As<Vector256<ulong>, Vector256<long>>(ref avSigned));
+
+                // Move borrow from Vector space to int
+                int borrow = Avx.MoveMask(Unsafe.As<Vector256<long>, Vector256<double>>(ref vBorrow));
+
+                // All zeros will cascade another borrow when borrow is subtracted from it
+                Vector256<ulong> vCascade = Avx2.CompareEqual(result, Vector256<ulong>.Zero);
+                // Move cascade from Vector space to int
+                int cascade = Avx.MoveMask(Unsafe.As<Vector256<ulong>, Vector256<double>>(ref vCascade));
+
+                // Use ints to work out the Vector cross lane cascades
+                // Move borrow to next bit and add cascade
+                borrow = cascade + 2 * borrow; // lea
+                // Remove cascades not effected by borrow
+                cascade ^= borrow;
+                // Choice of 16 vectors
+                cascade &= 0x0f;
+
+                // Lookup the borrows to broadcast to the Vectors
+                Vector256<ulong> cascadedBorrows = Unsafe.Add(ref Unsafe.As<byte, Vector256<ulong>>(ref MemoryMarshal.GetReference(ElementUtils.SBroadcastLookup)), cascade);
+
+                // Mark res as initalized so we can use it as left said of ref assignment
+                Unsafe.SkipInit(out res);
+                // Subtract the cascadedBorrows from the result
+                Unsafe.As<FE, Vector256<ulong>>(ref res) = Avx2.Subtract(result, cascadedBorrows);
+                return (borrow & 0b1_0000) != 0;
+            }
+            else
+            {
+                ref ulong rx = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in a));
+                ref ulong ry = ref Unsafe.As<FE, ulong>(ref Unsafe.AsRef(in b));
+                ulong borrow = 0ul;
+                ElementUtils.SubtractWithBorrow(rx, ry, ref borrow, out ulong res0);
+                ElementUtils.SubtractWithBorrow(Unsafe.Add(ref rx, 1), Unsafe.Add(ref ry, 1), ref borrow, out ulong res1);
+                ElementUtils.SubtractWithBorrow(Unsafe.Add(ref rx, 2), Unsafe.Add(ref ry, 2), ref borrow, out ulong res2);
+                ElementUtils.SubtractWithBorrow(Unsafe.Add(ref rx, 3), Unsafe.Add(ref ry, 3), ref borrow, out ulong res3);
+                res = new FE(res0, res1, res2, res3);
+                return borrow != 0;
+            }
         }
     }
 }
