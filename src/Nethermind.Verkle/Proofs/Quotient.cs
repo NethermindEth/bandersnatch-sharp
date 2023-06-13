18101feb53ae78a4c66a62efd07b0263a25d2058
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-06-13 10:11:42 +0530

    add poly aggreagation for proof calculation

--- src/Nethermind.Verkle/Proofs/Quotient.cs
+++ src/Nethermind.Verkle/Proofs/Quotient.cs
@@ -31,6 +31,28 @@ public static class Quotient
         return q;
     }
 
+    public static void ComputeQuotientInsideDomain(PreComputedWeights preComp, LagrangeBasis f, byte index, Span<FrE> quotient)
+    {
+        int domainSize = f.Evaluations.Length;
+
+        FrE[] inverses = preComp.DomainInv;
+        FrE[] aPrimeDomain = preComp.APrimeDomain;
+        FrE[] aPrimeDomainInv = preComp.APrimeDomainInv;
+
+        FrE y = f.Evaluations[index];
+
+        for (int i = 0; i < domainSize; i++)
+        {
+            if (i == index) continue;
+
+            int firstIndex = (i - index) < 0 ? (inverses.Length + (i - index)) : (i - index);
+            int secondIndex = (index - i) < 0 ? (inverses.Length + index - i) : (index - i);
+
+            quotient[i] = (f.Evaluations[i] - y) * inverses[firstIndex];
+            quotient[index] += (f.Evaluations[i] - y) * inverses[secondIndex] * aPrimeDomain[index] * aPrimeDomainInv[i];
+        }
+    }
+
     public static FrE[] ComputeQuotientOutsideDomain(PreComputedWeights preComp, LagrangeBasis f, FrE z, FrE y)
     {
         FrE[] domain = preComp.Domain;
