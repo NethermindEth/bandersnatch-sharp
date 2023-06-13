09379e0955924e22f39c61c53e1f0f792b064ae6
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-06-13 17:47:14 +0530

    remove unused func

--- src/Nethermind.Verkle/Proofs/Quotient.cs
+++ src/Nethermind.Verkle/Proofs/Quotient.cs
@@ -6,31 +6,6 @@ namespace Nethermind.Verkle.Proofs;
 
 public static class Quotient
 {
-    public static FrE[] ComputeQuotientInsideDomain(PreComputedWeights preComp, LagrangeBasis f, byte index)
-    {
-        int domainSize = f.Evaluations.Length;
-
-        FrE[] inverses = preComp.DomainInv;
-        FrE[] aPrimeDomain = preComp.APrimeDomain;
-        FrE[] aPrimeDomainInv = preComp.APrimeDomainInv;
-
-        FrE[] q = new FrE[domainSize];
-        FrE y = f.Evaluations[index];
-
-        for (int i = 0; i < domainSize; i++)
-        {
-            if (i == index) continue;
-
-            int firstIndex = (i - index) < 0 ? (inverses.Length + (i - index)) : (i - index);
-            int secondIndex = (index - i) < 0 ? (inverses.Length + index - i) : (index - i);
-
-            q[i] = (f.Evaluations[i] - y) * inverses[firstIndex];
-            q[index] += (f.Evaluations[i] - y) * inverses[secondIndex] * aPrimeDomain[index] * aPrimeDomainInv[i];
-        }
-
-        return q;
-    }
-
     public static void ComputeQuotientInsideDomain(PreComputedWeights preComp, LagrangeBasis f, byte index, Span<FrE> quotient)
     {
         int domainSize = f.Evaluations.Length;
