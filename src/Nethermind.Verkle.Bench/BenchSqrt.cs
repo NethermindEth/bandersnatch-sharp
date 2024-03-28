fb030098754ba394caccf514b9e03dc586a36bb0
Tanishq Jasoria <jasoriatanishq@gmail.com>
2024-03-28 12:08:15 +0530

    update benchmark

--- src/Nethermind.Verkle.Bench/BenchSqrt.cs
+++ src/Nethermind.Verkle.Bench/BenchSqrt.cs
@@ -9,7 +9,17 @@ namespace Nethermind.Verkle.Bench;
 [MemoryDiagnoser]
 public class BenchSqrt
 {
-    private FE _randomSquare;
+    [ParamsSource(nameof(SquareList))] public FE _randomSquare;
+    public static IEnumerable<FE> SquareList => _randomSquareListHex.Select(x => FE.FromBytesReduced(Convert.FromHexString(x)));
+
+    public static readonly string[] _randomSquareListHex =
+    [
+        "6D050190ABA180050907CECACEEAFB17A79680C697008E6EE4FE0141F55B4750",
+        "04B868D3637F0A880CF9E67A9C2E12AAE75862C0C978BD392F150E834C982139",
+        "BE7DC4158328B5278D9BBC41187010BF292CB3BA4FA54481797EBFA581B6FB58",
+        "890FCBA8BB6D091895AB1259FE374606EC713AC9D340138CC38165B736602220",
+        "97B4EF2EDA76503CF096A8D93AE53CB06FF06912104D7F3F7A8FFE911881A018",
+    ];
 
     [GlobalSetup]
     public void Setup()
@@ -25,14 +35,16 @@ public class BenchSqrt
     }
 
     [Benchmark(Baseline = true)]
-    public void SqrtOld()
+    public FE SqrtTonelliShanks()
     {
-        FE.Sqrt(_randomSquare, out FE sqrtOld);
+        FE.SqrtOld(_randomSquare, out FE sqrtOld);
+        return sqrtOld;
     }
 
     [Benchmark]
-    public void SqrtNew()
+    public FE SqrtTonelliShanksPreComputedTables()
     {
-        FE.SqrtNew(_randomSquare, out FE sqrtNew);
+        FE.Sqrt(_randomSquare, out FE sqrtNew);
+        return sqrtNew;
     }
 }
