b855c6d093b635bd2714135f6f91eee1816ee03f
Amirul Ashraf <asdacap@gmail.com>
2024-05-30 17:03:57 +0800

    More benchmark

--- src/Nethermind.Verkle.Bench/BenchCurveOps.cs
+++ src/Nethermind.Verkle.Bench/BenchCurveOps.cs
@@ -1,6 +1,7 @@
 // Copyright 2022 Demerzel Solutions Limited
 // Licensed under Apache-2.0.For full terms, see LICENSE in the project root.
 
+using System.Threading.Tasks.Dataflow;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using Nethermind.Verkle.Curve;
@@ -13,6 +14,13 @@ namespace Nethermind.Verkle.Bench;
 [MemoryDiagnoser]
 public class BenchCurveOps
 {
+    [Params(1, 2, 4, 8, 16, 32, 256)]
+    // [Params(256)]
+    public int InputSize { get; set; }
+
+    [Params(1, 4)]
+    public int Parallelism { get; set; }
+
     private readonly FrE[] _a;
 
     public BenchCurveOps()
@@ -33,6 +41,33 @@ public class BenchCurveOps
     [Benchmark]
     public void BenchmarkMultiScalarMul()
     {
-        Banderwagon.MultiScalarMul(Crs.BasisG, _a);
+        Banderwagon.MultiScalarMul(Crs.BasisG[..InputSize], _a[..InputSize], Parallelism);
+    }
+
+    [Benchmark]
+    public void BenchmarkScalarMul()
+    {
+        SpinLock accumulationLock = new SpinLock();
+        Banderwagon accumulation = Banderwagon.Identity;
+
+        ActionBlock<int> accumulator = new ActionBlock<int>((i) =>
+        {
+            Banderwagon multiplierd = Banderwagon.ScalarMul(Crs.BasisG[i], _a[i]);
+            bool locked = false;
+            accumulationLock.Enter(ref locked);
+            accumulation += multiplierd;
+            accumulationLock.Exit();
+        }, new ExecutionDataflowBlockOptions()
+        {
+            MaxDegreeOfParallelism = Parallelism,
+        });
+
+        for (int i = 0; i < InputSize; i++)
+        {
+            accumulator.Post(i);
+        }
+
+        accumulator.Complete();
+        accumulator.Completion.Wait();
     }
 }
